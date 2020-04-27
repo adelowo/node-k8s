eaab98b28d14f4152635cfd67e7a6a281068e3c9
Lanre Adelowo <yo@lanre.wtf>
2020-04-27 05:10:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const faker = require("faker");
+const app = express();
+
+const port = process.env.PORT || 5200;
+
+app.get("/profile", (req, res) => {
+  res.json({
+    status: true,
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phone: faker.phone.phoneNumber(),
+	  email: faker.internet.email(),
+	  avatar: faker.internet.avatar(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on ${port}`);
+});
+
